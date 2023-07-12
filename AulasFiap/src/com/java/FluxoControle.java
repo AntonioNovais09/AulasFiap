816b68705f32fa7f87d85211579e8aa5c27c3253
Antonio <Antonio@DESKTOP-CEIAAKE>
2023-07-12 10:00:12 -0300

    entrada e saida de dados

--- AulasFiap/src/com/java/FluxoControle.java
+++ AulasFiap/src/com/java/FluxoControle.java
@@ -1,27 +1,40 @@
 package com.java;
 
+import java.util.Scanner;
+
 public class FluxoControle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int idade = 85;
+		int idade = 0;
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Digite o nome: ");
+		String nome = sc.nextLine();
+		
+		System.out.println("Digite sua idade: ");
+		idade = sc.nextInt ();
+		
+		
 
 		if (idade < 0)
 			System.out.println("Idade Invalida");
 
-		if (idade >= 18 && idade < 50)	{
+		if (idade >= 18 && idade < 70)	{
 			System.out.println("Maior Idade");
-		} else if (idade >= 70 ) {
+			
+		} else if (idade >= 70 && idade <= 100 ) {
 			System.out.println("Terceira Idade");
-		 if (idade >=  100 ) {
+
+			if (idade >=  100 ) {
 				System.out.println("Centenria");
-		
+
 			}
 		} else	
 			System.out.println("Menor Idade");
 	}	
-	
+
 }
 
 
